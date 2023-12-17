7b2a7e9c448faa6afab621a0c1973941d0b7f4e7
Šimon Bernard <113086082+SimonBer7@users.noreply.github.com>
2023-12-17 20:10:06 +0100

    Add files via upload

--- tt/main.py
+++ tt/main.py
@@ -9,8 +9,43 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from itertools import permutations
+
+
+class Rozvrh:
+    def __init__(self, rozvrh_data):
+        self.rozvrh_data = rozvrh_data
+
+
+
+def generuj_varianty( rozvrh):
+
+    predmety_na_dny = [rozvrh.rozvrh_data[i:i + 10] for i in range(0, len(rozvrh.rozvrh_data), 10)]
+
+    for den in predmety_na_dny:
+        for permutace in permutations(den):
+            index+=1
+            yield list(permutace)
+
+
+def main():
+    # Předpokládáme, že máme již vytvořený rozvrh_data
+    rozvrh_data = ["M", "DS", "DS", "PSS", "PSS", "A", None, "TV", None, None,
+                   "PIS", "M", "PIS", "PIS", "TP", "A", "CJ", None, None, None,
+                   "CIT", "CIT", "WA", "DS", "PV", None, "PSS", None, None, None,
+                   "AM", "M", "WA", "WA", None, "A", "C", "PIS", "TV", None,
+                   "C", "A", "M", "PV", "PV", "AM", None, None, None, None]
+
+    rozvrh = Rozvrh(rozvrh_data)
+
+    # Spustíme generátor variant rozvrhu
+    for variant in generuj_varianty(rozvrh):
+        print(variant)
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tt/timetable.py
+++ tt/timetable.py
@@ -1,4 +1,7 @@
 from itertools import permutations
+import time
+import multiprocessing
+
 
 class Timetable:
     def __init__(self):
@@ -14,14 +17,78 @@ class Timetable:
         perm = permutations(["M", "DS", "DS", "PSS", "PSS", "A", None, "TV", None, None, "PIS", "M", "PIS", "PIS", "TP", "A", "CJ", None, None, None, "CIT", "CIT", "WA", "DS", "PV", None, "PSS", None, None, None, "AM", "M", "WA", "WA", None, "A", "C", "PIS", "TV", None, "C",   "A", "M", "PV", "PV", "AM", None, None, None, None])
         return perm
 
+    def get_permutation_monday(self):
+        tmp = list(permutations(self.table["Monday"]))
+        return tmp
+
+    def get_permutation_tuesday(self):
+        tmp = list(permutations(self.table["Tuesday"]))
+        return tmp
+
+    def get_permutation_wednesday(self):
+        tmp = list(permutations(self.table["Wednesday"]))
+        return tmp
+
+    def get_permutation_thursday(self):
+        tmp = list(permutations(self.table["Thursday"]))
+        return tmp
+
+    def get_permutation_friday(self):
+        tmp = list(permutations(self.table["Friday"]))
+        return tmp
+
+    def set_table(self):
+        self.table["Monday"] = list(self.get_permutation_monday().pop(0))
+        self.table["Tuesday"] = list(self.get_permutation_tuesday().pop(0))
+        self.table["Wednesday"] = list(self.get_permutation_wednesday().pop(0))
+        self.table["Thursday"] = list(self.get_permutation_thursday().pop(0))
+        self.table["Friday"] = list(self.get_permutation_friday().pop(0))
+
+
+
+
+
+
+
 t = Timetable()
 
+index = 0
+while True:
+    print(list(t.get_permutation_monday().pop(0)))
+    print(list(t.get_permutation_tuesday().pop(0)))
+    print(list(t.get_permutation_wednesday().pop(0)))
+    print(list(t.get_permutation_thursday().pop(0)))
+    print(list(t.get_permutation_friday().pop(0)))
+    print(index)
+    index+= 1
+
+
+
+
+if __name__ == "__main__":
+
+    start = time.time()
+
+
+
+
+
+"""
 # Print original timetable
-#for day in t.table:
-#    print(f"{day}: {t.table[day]}")
+print("Puvodni rozvrh:")
+for day in t.table:
+    print(f"{t.table[day]}")
+print("_______________________________________________________________")
 
 index = 0
-for i in t.generate_permutations():
-    print(i)
-    index+=1
+while True:
+    t.set_table()
+    for day in t.table:
+        print(f"{t.table[day]}")
     print(index)
+    index+=1
+"""
+
+
+
+
