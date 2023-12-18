a2544bdc8bcb906724f2424bcf6d8d47b39c40fb
Šimon Bernard <113086082+SimonBer7@users.noreply.github.com>
2023-12-18 22:42:09 +0100

    Add files via upload

--- tt/main.py
+++ tt/main.py
@@ -1,51 +1,25 @@
-# This is a sample Python script.
+import multiprocessing
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-from itertools import permutations
-
-
-class Rozvrh:
-    def __init__(self, rozvrh_data):
-        self.rozvrh_data = rozvrh_data
-
-
-
-def generuj_varianty( rozvrh):
-
-    predmety_na_dny = [rozvrh.rozvrh_data[i:i + 10] for i in range(0, len(rozvrh.rozvrh_data), 10)]
-
-    for den in predmety_na_dny:
-        for permutace in permutations(den):
-            index+=1
-            yield list(permutace)
-
-
-def main():
-    # Předpokládáme, že máme již vytvořený rozvrh_data
-    rozvrh_data = ["M", "DS", "DS", "PSS", "PSS", "A", None, "TV", None, None,
-                   "PIS", "M", "PIS", "PIS", "TP", "A", "CJ", None, None, None,
-                   "CIT", "CIT", "WA", "DS", "PV", None, "PSS", None, None, None,
-                   "AM", "M", "WA", "WA", None, "A", "C", "PIS", "TV", None,
-                   "C", "A", "M", "PV", "PV", "AM", None, None, None, None]
-
-    rozvrh = Rozvrh(rozvrh_data)
-
-    # Spustíme generátor variant rozvrhu
-    for variant in generuj_varianty(rozvrh):
-        print(variant)
-
+def worker():
+    while True:
+        pass
 
 if __name__ == "__main__":
-    main()
+    num_cores = multiprocessing.cpu_count()
+    processes = []
 
+    for _ in range(num_cores):
+        process = multiprocessing.Process(target=worker)
+        process.start()
+        processes.append(process)
 
+    # Necháme program běžet po dobu 5 sekund
+    time.sleep(50)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Zastavíme všechny procesy
+    for process in processes:
+        process.terminate()
+        process.join()
+
+    print("Zatížení procesorových jader bylo ukončeno.")
